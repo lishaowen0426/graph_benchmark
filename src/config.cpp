08a2ff5d28e9ae165bb7d185d0aad2c32cb42a81
lsw <litony0426@gmail.com>
2022-04-18 05:48:23 +0000

    thread id

--- src/config.cpp
+++ src/config.cpp
@@ -4,11 +4,12 @@
 
 void pinning_observer::on_scheduler_entry(bool worker){
     int s;
-    int id = oneapi::tbb::this_task_arena::current_thread_index();
+    int id = oneapi::tbb::this_task_arena::current_thread_index()%THREADS;
     CPU_ZERO(&mask);
     CPU_SET(id,&mask);
     s = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &mask);
     if(s != 0){
+        printf("id: %d\n",id);
         handle_error_en(s,"set affinity failed");
     }
 }
