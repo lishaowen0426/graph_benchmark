cb28624350c93240be4a10404cc81959e290e7d6
lsw <litony0426@gmail.com>
2022-04-26 11:14:21 +0000

    pointer_chasing

--- pointer_chasing/main.cpp
+++ pointer_chasing/main.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char** argv){
 
     if(argc < 4){
-        printf("./chasing [buffer size in bytes] [count] [cas_success]");
+        printf("./chasing [buffer size in bytes] [count] ");
         exit(0);
     }
 #ifdef PMEM
@@ -20,23 +20,16 @@ int main(int argc, char** argv){
     size_t buffer_sz;
     buffer_sz = atol(argv[1]); 
     size_t count = atol(argv[2]);
-    //count = (count > (1<<30))? count: (1<<30);
-    bool cas_success = atoi(argv[3]);
-    bool shuffle = !cas_success;
-    if(cas_success){
-        printf("Benchmark cas_success\n");
-    }else{
-        printf("Benchmark cas_fail\n");
-    }
+    count = (count > (1<<30))? count: (1<<30);
 
-    void* buffer = create_random<PAD_SIZE>( buffer_sz, len, shuffle);
+    void* buffer = create_random<PAD_SIZE>( buffer_sz, len, true);
     if(buffer_sz < (1024*1024))
         printf(" buffer size: %.2fkb\n",  ((float)buffer_sz/(1024.0)));
     else
         printf(" buffer size: %.2fmb\n",  ((float)buffer_sz/(1024.0*1024.0)));
     uint64_t start, stop;
     rdtscll(start);
-    cas_hub(buffer, len, count, cas_success, PAD_SIZE+sizeof(void*));
+    read_chase(buffer,count);
     rdtscll(stop);
     printf("Chasing time: %.4f ns\n", 1000000000.0*CYCLES_TO_SEC(start,stop, get_cpu_freq())/(float)count);
 
